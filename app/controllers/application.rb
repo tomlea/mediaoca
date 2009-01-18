a3e28df37afac545ace8fc7388f018b583aa863b
Tom Lea <contrib@tomlea.co.uk>
2009-01-18 21:03:51 +0000

    UI Template Started.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -8,6 +8,11 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '02007f33c5329276593e99411e45c8b5'
   
+  layout :select_layout
+  def select_layout
+    "master"
+  end
+  
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
