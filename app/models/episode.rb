52d6139d703f84b3be54c7b417d1589328242d01
Tom Lea <contrib@tomlea.co.uk>
2009-02-07 19:57:50 +0000

    clean up.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,40 +1,42 @@
 class Episode < ActiveRecord::Base
   EPISODE_MATCHERS = [/([0-9]{1,2})x([0-9]{1,2})/i, /s([0-9]{1,2})e([0-9]{1,2})/i]
   belongs_to :show
-  
-  def self.file_digest(file)
-    Digest::MD5.hexdigest(file)
-  end
-  
-  def self.find_or_create_by_filename(filename)
-    find_or_create_by_hash_code(file_digest(filename))
-  end
-  
-  def self.for(name)
-    returning find_or_create_by_filename(name) do |e|
-      e.filename = name
-      e.show ||= Show.guess_show(e)
-    end
-  end
-  
-  def self.clenseables
-    YAML.load(File.open(File.join(Rails.root, "config", "clenseables.yml")))
-  end
-  
-  def self.media_paths
-    @media_paths ||= YAML.load(File.open(File.join(Rails.root, "config", "media_paths.yml")))
-  end
 
-  def self.all
-    if @episodes and @episodes_updated > Time.now - 5.minuits
-      @episodes
-    else
-      @episodes_updated = Time.now
-      @episodes = media_paths.inject([]){|acc, path|
-        acc + Dir.glob("#{path}/**/*.{avi,wmv,divx,mkv,ts,mov,mp4}")
-      }.map{|filename|
-        Episode.for(filename)
-      }
+  class << self
+    def file_digest(file)
+      Digest::MD5.hexdigest(file)
+    end
+
+    def find_or_create_by_filename(filename)
+      find_or_create_by_hash_code(file_digest(filename))
+    end
+
+    def for(name)
+      returning find_or_create_by_filename(name) do |e|
+        e.filename = name
+        e.show ||= Show.guess_show(e)
+      end
+    end
+
+    def clenseables
+      YAML.load(File.open(File.join(Rails.root, "config", "clenseables.yml")))
+    end
+
+    def media_paths
+      @media_paths ||= YAML.load(File.open(File.join(Rails.root, "config", "media_paths.yml")))
+    end
+
+    def all
+      if @episodes and @episodes_updated > Time.now - 5.minuits
+        @episodes
+      else
+        @episodes_updated = Time.now
+        @episodes = media_paths.inject([]){|acc, path|
+          acc + Dir.glob("#{path}/**/*.{avi,wmv,divx,mkv,ts,mov,mp4}")
+        }.map{|filename|
+          Episode.for(filename)
+        }
+      end
     end
   end
 
