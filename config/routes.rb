b85d7e09ea97e97fe29fc71b2a7c328d90e810b8
Tom Lea <contrib@tomlea.co.uk>
2009-02-07 19:37:17 +0000

    Got so wrapped up in restful routes I forgott I wasnt using them!

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "episodes", :collection => {:stop => :post, :pause => :post, :seen => :post, :play => :post}
+  map.root :controller => "episodes"
 
   # See how all your routes lay out with "rake routes"
 
