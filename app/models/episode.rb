f686bf3dc8b2e1489446d5e167a650c215795ff4
Tom Lea <contrib@tomlea.co.uk>
2009-02-11 22:16:38 +0000

    We are still finding episodes by hash, but try a hash that's just the filename.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -8,7 +8,13 @@ class Episode < ActiveRecord::Base
     end
 
     def find_or_create_by_filename(filename)
-      find_or_create_by_hash_code(file_digest(filename))
+      hash = file_digest(File.basename(filename))
+      if ep = find_or_by_hash_code(file_digest(filename))
+        ep.update_attribute(:hash_code, hash)
+        ep
+      else
+        find_or_create_by_hash_code(hash)
+      end
     end
 
     def for(name)
@@ -38,12 +44,26 @@ class Episode < ActiveRecord::Base
         }
       end
     end
+
+    def scan_for_episodes!
+      media_paths.each{|path|
+        Dir.glob("#{path}/**/*.{avi,wmv,divx,mkv,ts,mov,mp4}")
+      }.flatten.map{|filename|
+        Episode.for(filename)
+      }
+    end
+
+    def clean_up_deleted_episodes!
+      @episodes.each do |episode|
+        episode.destroy! if episode.filename.nil? or not File.exist?(episode.filename)
+      end
+    end
   end
 
   def to_param
     hash_code
   end
-  
+
   def name
     returning File.basename(filename) do |name|
       name[/\.[^.]+$/] = ""
@@ -58,15 +78,15 @@ class Episode < ActiveRecord::Base
       end
     end
   end
-  
+
   def series
     series_and_episode && series_and_episode.first
   end
-  
+
   def episode
     series_and_episode && series_and_episode.last
   end
-  
+
   def series_and_episode
     if EPISODE_MATCHERS.any?{|re| filename =~ re }
       [$1.to_i, $2.to_i]
@@ -74,15 +94,15 @@ class Episode < ActiveRecord::Base
       nil
     end
   end
-  
+
   def seen
     last_watched?
   end
-  
+
   def seen?
     last_watched?
   end
-  
+
   def seen!
     self.seen = true
   end
