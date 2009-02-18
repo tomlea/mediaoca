d7e62ae3d59562dba68ab5a3f733a19973d4b7a2
Tom Lea <contrib@tomlea.co.uk>
2009-02-18 00:10:12 +0000

    View cleanup. Moved to HAML.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def format_disk_usage(disk_usage)
+    available = disk_usage.available / 1024
+    total = disk_usage.total / 1024
+    "<strong>#{available}</strong>/<strong>#{total}</strong>"
+  end
 end
--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -1,14 +1,30 @@
 require "digest/md5"
 module EpisodesHelper
-  def link_to_play(text, episode, options = {})
-    link_to_remote(text, {:url => {:action => "play", :episode => episode}}, options)
+  def link_to_play(episode)
+    link_to_remote "play", {:url => {:action => "play", :episode => episode}}, {:class => "play"}
   end
 
-  def link_to_seen(text, episode, options = {})
-    link_to_remote(text, {:url => {:action => "seen", :episode => episode}}, options)
+  def link_to_seen(episode)
+    link_to_remote "toggle seen", {:url => {:action => "seen", :episode => episode}}, {:class => "seen"}
   end
-  
-  def file_digest(file)
-    Digest::MD5.hexdigest(file)
+
+  def link_to_stop
+    link_to_remote "stop", :url => {:controller => :episodes, :action => "stop"}, :html => {:class => "stop"}
+  end
+
+  def link_to_pause
+    link_to_remote "play/pause", :url => {:controller => :episodes, :action => "pause"}
+  end
+
+  def currently_playing?(episode = :anything)
+    @currently_playing_episode == episode or episode == :anything and not @currently_playing_episode.nil?
+  end
+
+  def currently_playing_name
+    h File.basename(@currently_playing_episode.filename)
+  end
+
+  def paused?
+    @paused
   end
 end
