65d6011cd98dcf7c46ca9f35df26feb0961e6a3a
Tom Lea <contrib@tomlea.co.uk>
2009-02-19 22:20:37 +0000

    White-space was annoying.

--- app/models/enqueue.rb
+++ app/models/enqueue.rb
@@ -2,13 +2,13 @@ class Enqueue < Struct.new(:newzbin_id)
   def initialize(options = {})
     update_attributes(options)
   end
-  
+
   def update_attributes(keys_and_values)
     keys_and_values.each do |key, value|
       self.send("#{key}=", value)
     end
   end
-  
+
   def id
     nil
   end
--- app/models/hellanzb.rb
+++ app/models/hellanzb.rb
@@ -2,27 +2,27 @@ require 'timeout'
 class Hellanzb
   HELLA_URL = "http://hellanzb:changeme@localhost:8760"
   HELLA_BIN = `which hellanzb`.chomp
-  
+
   require 'xmlrpc/client'
   attr_reader :config_file, :prefix_dir, :queue, :dest_dir
 
-  def initialize 
+  def initialize
     begin
       @server ||= XMLRPC::Client.new2(HELLA_URL)
       # Create a method accessor for each of the server methods
       @server.call('system.listMethods').each do |m|
-        if m.eql?("down") or m.eql?("up") or m.eql?("dequeue") or 
+        if m.eql?("down") or m.eql?("up") or m.eql?("dequeue") or
            m.eql?("last") or m.eql?("next") or m.eql?("force") or
            m.eql?("enqueue") or m.eql?("enqueueurl") or m.eql?("enqueuenewzbin")
 
-          self.class.send(:define_method, m) {|id| @server.call(m,id)} 
+          self.class.send(:define_method, m) {|id| @server.call(m,id)}
         else
           self.class.send(:define_method, m) {@server.call(m)}
         end
       end
 
       status.each do |s|
-        if s[0].eql?('currently_downloading') or 
+        if s[0].eql?('currently_downloading') or
            s[0].eql?('currently_processing')then
           self.class.send(:define_method, s[0]) {@server.call("status")[s[0]][0]}
         else
@@ -31,7 +31,7 @@ class Hellanzb
       end
     end
   end
-  
+
   def self.start_server
     Timeout.timeout(1) do
       system(HELLA_BIN+" -D")
