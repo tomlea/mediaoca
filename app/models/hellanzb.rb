16a27874362ca20ad071bc89588ce4114b43c188
Tom Lea <commit@tomlea.co.uk>
2009-04-06 00:35:49 +0100

    begin...end for no reason?

--- app/models/hellanzb.rb
+++ app/models/hellanzb.rb
@@ -7,27 +7,25 @@ class Hellanzb
   attr_reader :config_file, :prefix_dir, :queue, :dest_dir
 
   def initialize
-    begin
-      @server ||= XMLRPC::Client.new2(HELLA_URL)
-      # Create a method accessor for each of the server methods
-      @server.call('system.listMethods').each do |m|
-        if m.eql?("down") or m.eql?("up") or m.eql?("dequeue") or
-           m.eql?("last") or m.eql?("next") or m.eql?("force") or
-           m.eql?("enqueue") or m.eql?("enqueueurl") or m.eql?("enqueuenewzbin")
+    @server ||= XMLRPC::Client.new2(HELLA_URL)
+    # Create a method accessor for each of the server methods
+    @server.call('system.listMethods').each do |m|
+      if m.eql?("down") or m.eql?("up") or m.eql?("dequeue") or
+         m.eql?("last") or m.eql?("next") or m.eql?("force") or
+         m.eql?("enqueue") or m.eql?("enqueueurl") or m.eql?("enqueuenewzbin")
 
-          self.class.send(:define_method, m) {|id| @server.call(m,id)}
-        else
-          self.class.send(:define_method, m) {@server.call(m)}
-        end
+        self.class.send(:define_method, m) {|id| @server.call(m,id)}
+      else
+        self.class.send(:define_method, m) {@server.call(m)}
       end
+    end
 
-      status.each do |s|
-        if s[0].eql?('currently_downloading') or
-           s[0].eql?('currently_processing')then
-          self.class.send(:define_method, s[0]) {@server.call("status")[s[0]][0]}
-        else
-          self.class.send(:define_method, s[0]) {@server.call("status")[s[0]]}
-        end
+    status.each do |s|
+      if s[0].eql?('currently_downloading') or
+         s[0].eql?('currently_processing')then
+        self.class.send(:define_method, s[0]) {@server.call("status")[s[0]][0]}
+      else
+        self.class.send(:define_method, s[0]) {@server.call("status")[s[0]]}
       end
     end
   end
