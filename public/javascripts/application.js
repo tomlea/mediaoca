22d419992fc9f18fe0429ac8307a74c1a44fb226
Tom Lea <commit@tomlea.co.uk>
2009-04-29 00:38:52 +0100

    Only respond to clicks on target.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,8 @@
 function showDetailLightbox(){
-  $('detail_lightbox_backdrop').observe('click', function(){
-    $('detail_lightbox_backdrop').hide();
-  });
+  $('detail_lightbox_backdrop').observe('click', function(e){
+    if(e.findElement() == $('detail_lightbox_backdrop'))
+      $('detail_lightbox_backdrop').hide();
+  }, false);
   
   $('detail_lightbox_backdrop').show();
 }
