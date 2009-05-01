f17930f8a2003be737701ac5ff5808e6712c1a39
Tom Lea <commit@tomlea.co.uk>
2009-05-01 19:49:36 +0100

    Progress bars are cool.

--- app/helpers/hellanzb_helper.rb
+++ app/helpers/hellanzb_helper.rb
@@ -2,4 +2,8 @@ module HellanzbHelper
   def hellanzb_progress(progress, size)
     "<strong>#{progress}%</strong> of <strong>#{size}</strong>mb"
   end
+
+  def hellanzb_progress_chart(progress)
+    %{<div class="progress_bar"><div style="width: #{progress}%;">#{progress}%</div></div>}
+  end
 end
